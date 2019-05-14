c9e310154b7b35785ec0debfd32b44e29ab86e72
Stephen Tallowitz <stephen.tallowitz@petzi.ch>
2019-05-14 16:23:52 +0200

    Add Makefile and test file

---
+++ test/test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * A (valid) test file for php-cs
+ *
+ * @category None
+ * @package  None
+ * @author   Test Author <test@example.org>
+ * @license  https://github.com/petzich/docker-pre-commit-php/blob/master/LICENSE MIT
+ * @link     None
+ * */
+echo "hello world";
+
+?>
